d7dfb0917970b085cb545447d9954057de4cc2fe
Jan Stevens <jan@playpass.be>
2020-08-19 17:23:58 +0200

    Fetching and caching repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import classNames from 'classnames'
 
+import { fetchPopularRepos } from '../utils/api'
+
 function LanguagesNav({ selected, onUpdateLanguages }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'Go']
 
@@ -27,16 +29,47 @@ export default class Popular extends React.Component {
     super(props)
     this.state = {
       selectedLanguage: 'All',
+      repos: {},
+      error: null,
     }
     this.updateLanguage = this.updateLanguage.bind(this)
+    this.isLoading = this.isLoading.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
   }
 
   updateLanguage(selectedLanguage) {
-    this.setState({ selectedLanguage })
+    this.setState({ selectedLanguage, error: null })
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) =>
+          this.setState(({ repos }) => ({
+            repos: {
+              [selectedLanguage]: data,
+              ...repos,
+            },
+            error: null,
+          }))
+        )
+        .catch(() => {
+          console.warn('Error fetching repos: ', error)
+          this.setState({
+            error: `There was an error fetching the repositories.`,
+          })
+        })
+    }
+  }
+
+  isLoading() {
+    const { selectedLanguage, error, repos } = this.state
+    return !repos[selectedLanguage] && error === null
   }
 
   render() {
-    const { selectedLanguage } = this.state
+    const { selectedLanguage, repos, error } = this.state
 
     return (
       <>
@@ -44,6 +77,12 @@ export default class Popular extends React.Component {
           selected={selectedLanguage}
           onUpdateLanguages={this.updateLanguage}
         />
+
+        {this.isLoading() && <p>Loading</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
       </>
     )
   }
