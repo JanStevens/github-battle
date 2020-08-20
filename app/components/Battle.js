8b4ece28d2c11503aba3ccfd22d86ef4d36e75b4
Jan Stevens <jan@playpass.be>
2020-08-20 07:32:32 +0200

    Add Player Input component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -33,6 +33,52 @@ const Instructions = () => {
   )
 }
 
+class PlayerInput extends React.Component {
+  state = {
+    username: '',
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.onSubmit(this.state.username)
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  render() {
+    const { username } = this.state
+    return (
+      <form className={'column player'} onSubmit={this.handleSubmit}>
+        <label htmlFor={'username'} className={'player-label'}>
+          {this.props.label}
+        </label>
+        <div className={'row player-inputs'}>
+          <input
+            type={'text'}
+            id={'username'}
+            className={'input-light'}
+            placeholder={'github username'}
+            autoComplete={'off'}
+            value={username}
+            onChange={this.handleChange}
+          />
+          <button
+            className={'btn dark-btn'}
+            type={'submit'}
+            disabled={!username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    )
+  }
+}
+
 export default class Battle extends React.Component {
   render() {
     return (
