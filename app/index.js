21e01b12439a2c5484e5ad9ed1c90fe768c6698f
Jan Stevens <jan@playpass.be>
2020-08-24 13:30:12 +0200

    Add routing with react router

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 import React, { useCallback, useMemo, useState } from 'react'
 import ReactDOM from 'react-dom'
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 import Nav from './components/Nav'
-
 import { ThemeProvider } from './contexts/theme'
 
 const App = () => {
@@ -24,14 +25,17 @@ const App = () => {
   )
 
   return (
-    <ThemeProvider value={themeProviderValue}>
-      <div className={theme}>
-        <div className={'container'}>
-          <Nav />
-          <Battle />
+    <Router>
+      <ThemeProvider value={themeProviderValue}>
+        <div className={theme}>
+          <div className={'container'}>
+            <Nav />
+            <Route exact path={'/'} component={Popular} />
+            <Route path={'/battle'} component={Battle} />
+          </div>
         </div>
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </Router>
   )
 }
 
